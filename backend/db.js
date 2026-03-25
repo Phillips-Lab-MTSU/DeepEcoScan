d087e79e1ead3c1037ea650ee7d62eaafa15ba2d
Haynie <yoitzgametime@gmail.com>
2026-03-25 06:51:23 -0500

    Ensuring current db.js is up to date

--- backend/db.js
+++ backend/db.js
@@ -5,11 +5,11 @@ async function connectDB() {
   const uri = process.env.MONGO_URI;
   if (!uri) throw new Error("MONGO_URI is not set");
 
-  // Helps avoid "strictQuery" warnings depending on Mongoose version
   mongoose.set("strictQuery", true);
 
   await mongoose.connect(uri);
-  console.log("Mongo connected");
+
+  console.log("CONNECTED TO DB:", mongoose.connection.name);
 }
 
 module.exports = { connectDB };
